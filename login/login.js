b2273766f4a9a97b9cf192564fe43607e8ebbef7
peaches007 <nnuola5@gmail.com>
2021-07-15 21:07:46 +0100

    refactored the code

---
+++ login/login.js
@@ -0,0 +1,44 @@
+const loginForm = document.querySelector("#loginForm");
+
+loginForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const url = "https://stocka-zuri-api.herokuapp.com/auth/login/";
+
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#pwd").value;
+
+    // const formData = new FormData(this);
+
+    try {
+        const response = await fetch (url, {
+            method: "POST",
+            body: JSON.stringify ({
+                "email": email,
+                "password": password
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then ((response) => {
+            console.log(response.json());
+            localStorage.setItem("token", response.token);
+            console.log(response.status);
+
+            if (response.ok) {
+                window.location.href="../user-dashboard/index.html"
+            }
+            else {
+                alert(response.error);
+            }
+        
+        })
+        .then (data => {
+            console.log(data);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+});
